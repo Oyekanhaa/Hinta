bde4370c4c734c886c7b9cd45ee7d1d77bf8f3c1
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-02-11 18:50:43 +0530

    Update start.py

--- PURVIMUSIC/utils/inline/start.py
+++ PURVIMUSIC/utils/inline/start.py
@@ -8,7 +8,7 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=f"<tg-emoji emoji-id=\"{5219943216781995020}\">🔥</tg-emoji> Add me", url=f"https://t.me/{app.username}?startgroup=true"
+                text=f"<tg-emoji emoji-id=\"{5219943216781995020}\">⚡</tg-emoji> Add me", url=f"https://t.me/{app.username}?startgroup=true"
             ),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         
