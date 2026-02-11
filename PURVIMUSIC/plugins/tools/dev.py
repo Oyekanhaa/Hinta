8b554b921a5c853511068fe69b6dc232191ada13
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-02-11 23:53:24 +0530

    Update dev.py

--- PURVIMUSIC/plugins/tools/dev.py
+++ PURVIMUSIC/plugins/tools/dev.py
@@ -31,13 +31,13 @@ async def edit_or_reply(msg: Message, **kwargs):
 
 @app.on_edited_message(
     filters.command("eval")
-    & filters.user(OWNER_ID)
+
     & ~filters.forwarded
     & ~filters.via_bot
 )
 @app.on_message(
     filters.command("eval")
-    & filters.user(OWNER_ID)
+    
     & ~filters.forwarded
     & ~filters.via_bot
 )
