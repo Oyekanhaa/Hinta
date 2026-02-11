89553b7a4dc024dcbdbef5a3ddb19dbb7814bdfa
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-02-11 18:24:06 +0530

    Update start.py

--- PURVIMUSIC/utils/inline/start.py
+++ PURVIMUSIC/utils/inline/start.py
@@ -1,35 +1,20 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
-async def start_panel(update, context):
-    keyboard = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text="Invite 🤙",
-                    url=config.SUPPORT_CHAT,
-                    api_kwargs={"icon_custom_emoji_id": "5219943216781995020"}
-                ),
-            ]
-        ]
-    )
-
-    await update.message.reply_text(
-        "Click below!",
-        reply_markup=keyboard
-    )
-    
-
-
 from pyrogram.types import InlineKeyboardButton
 
 import config
 from PURVIMUSIC import app
 
 
-
-
-
-
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            api_kwargs={"icon_custom_emoji_id": "5219943216781995020"}
+        ],
+    ]
+    return buttons
 
 
 def private_panel(_):
