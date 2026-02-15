a5a303338ab18abeb1c7956310bacea5b513784c
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-02-15 17:42:50 +0530

    Update tagall.py

--- PURVIMUSIC/plugins/tools/tagall.py
+++ PURVIMUSIC/plugins/tools/tagall.py
@@ -5,5 +5,7 @@ from pyrogram.types import Message
 @app.on_message(filters.command("all") & filters.group)
 async def tagall(client, message: Message):
     admins = [admin.user.id async for admin in app.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS)]
-    if message.from_user.id in admins:
-        await message.reply(f"{' '.join([f'@{m.user.username}' if m.user.username else f'[{m.user.first_name}](tg://user?id={m.user.id})' async for m in app.get_chat_members(message.chat.id)])}\n\n{message.text.split(None, 1)[1] if len(message.text.split()) > 1 else ''}")
+    if message.from_user.id not in admins: return await message.reply("Only admins can use this command")
+    members = [f"[{m.user.first_name}](tg://user?id={m.user.id})" async for m in app.get_chat_members(message.chat.id) if not m.user.is_bot]
+    text = message.text.split(None, 1)[1] if len(message.text.split()) > 1 else ""
+    await message.reply(f"{' '.join(members)}\n\n{text}")
