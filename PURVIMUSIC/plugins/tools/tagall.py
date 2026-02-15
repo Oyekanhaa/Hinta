d8756e748186b82e3f77c4ddc966e296ee42c95d
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-02-15 17:24:12 +0530

    Update tagall.py

--- PURVIMUSIC/plugins/tools/tagall.py
+++ PURVIMUSIC/plugins/tools/tagall.py
@@ -6,4 +6,4 @@ from pyrogram.types import Message
 async def tagall(client, message: Message):
     admins = [admin.user.id async for admin in app.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS)]
     if message.from_user.id in admins:
-        await message.reply(f"{' '.join([f'@{m.user.username}' if m.user.username else f'[{m.user.first_name}](tg://user?id={m.user.id})' async for m in client.get_chat_members(message.chat.id)])}\n\n{message.text.split(None, 1)[1] if len(message.text.split()) > 1 else ''}")
+        await message.reply(f"{' '.join([f'@{m.user.username}' if m.user.username else f'[{m.user.first_name}](tg://user?id={m.user.id})' async for m in app.get_chat_members(message.chat.id)])}\n\n{message.text.split(None, 1)[1] if len(message.text.split()) > 1 else ''}")
